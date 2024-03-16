fd09dd5172499eda684740716d6d835637a22ca0
Rene Marinho <152930400+renema@users.noreply.github.com>
2024-03-15 21:41:20 -0300

    Create Atividade02.java
    Atividade02

---
+++ Atividade02.java
@@ -0,0 +1,12 @@
+package aula02;
+
+public class Atividade02 {
+public static void main (String[] args) {
+	String nome = "Rene";
+	int idade = 38;
+	int BatimentoCardiaco = 220 - idade;
+	
+	System.out.println("O nome da pessoa é"  +nome+"  tem "+idade+"  anos e  Batimento esta  "+BatimentoCardiaco);
+	
+}
+}
